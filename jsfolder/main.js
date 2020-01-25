ad1b8b4e1e93df12718a86c9ec2d1ba2306d5118
Kindeme <fkounoudji@gmail.com>
2020-01-24 22:43:04 -0600

    some update

--- jsfolder/main.js
+++ jsfolder/main.js
@@ -4,3 +4,7 @@ const navLinks = document.getElementsByClassName("nav-links")[0];
 toggleButton.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+const copyDate = document.querySelector("#date");
+const date = new Date();
+copyDate.innerHTML = date.getFullYear();
