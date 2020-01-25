372b434f21587fe0d41d3631f79a07e4354e62d6
Kindeme <fkounoudji@gmail.com>
2020-01-25 11:54:01 -0600

    scroll smooth

--- jsfolder/main.js
+++ jsfolder/main.js
@@ -4,7 +4,7 @@ const copyDate = document.querySelector("#date");
 const date = new Date();
 copyDate.innerHTML = date.getFullYear();
 
-//navbar
+//navbar reponsible setup
 const btn = document.querySelector(".nav-btn");
 const navLinks = document.querySelector(".nav-links");
 console.log(btn);
@@ -20,4 +20,40 @@ btn.addEventListener("click",function(){
         navLinks.classList.add("show");
         btn.classList.add("change");
     }
+});
+
+// ***********************fixed navbar*******************
+const navbar = document.querySelector(".header");
+// add event listener to browser window
+
+window.addEventListener("scroll",() =>{
+
+ if(window.pageYOffset > 92){
+     navbar.classList.add("fixed");
+ }
+ else{
+    navbar.classList.remove("fixed");
+ }
+});
+
+
+// ***********************scrollLinks*******************
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(link =>{
+    link.addEventListener("click", e =>{
+        e.preventDefault();
+        navLinks.classList.remove("show");
+        btn.classList.remove("change");
+        const id = e.target.getAttribute("href").slice(1);
+        const elment = document.getElementById(id);
+
+        // window scrollTo
+        window.scrollTo({
+            left:0;
+            top:element.offsetTop,
+            behavior:"smooth"
+
+        });
+    });
 });
