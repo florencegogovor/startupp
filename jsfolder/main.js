476eda4566033b5eaf8e668c98c8d016e85b4d52
Kindeme <fkounoudji@gmail.com>
2020-01-25 00:02:00 -0600

    navbar

--- jsfolder/main.js
+++ jsfolder/main.js
@@ -1,5 +1,21 @@
 
-
+// copyright year
 const copyDate = document.querySelector("#date");
 const date = new Date();
 copyDate.innerHTML = date.getFullYear();
+
+//navbar
+const btn = document.querySelector(".nav-btn");
+const navLinks = document.querySelector(".nav-links");
+console.log(btn);
+console.log(navLinks);
+
+btn.addEventListener("click",function(){
+
+    let value = navLinks.classList.contains("show");
+    if(value){
+        navLinks.classList.remove("show");
+    }else{
+        navLinks.classList.add("show");
+    }
+});
