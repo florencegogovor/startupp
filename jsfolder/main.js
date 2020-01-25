792f73a13c6c5e7682308633df0d752c1992407a
Kindeme <fkounoudji@gmail.com>
2020-01-24 23:24:41 -0600

    navbar

--- jsfolder/main.js
+++ jsfolder/main.js
@@ -1,9 +1,4 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navLinks = document.getElementsByClassName("nav-links")[0];
 
-toggleButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
 
 const copyDate = document.querySelector("#date");
 const date = new Date();
